8d51ad97cc1bf64424d8377803460a3b49aa7aab
珏铭 <fengjueming@gmail.com>
2017-04-26 12:53:16 +0900

    新版

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+$username = $_POST["username"];
+$password = $_POST["password"];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,'https://yestudy.komazawa-u.ac.jp/2017/login/index.php');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,"username=" . $username . "&password=" . $password );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+$result = curl_exec($ch);
+curl_close($ch);
+if(strpos($result,'testsession') > 0){
+echo "Success";
+    exit;
+}
+?>
