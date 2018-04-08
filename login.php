e6026859c8856a738d02e591f7197ce07eb00fae
珏铭 <fengjueming@gmail.com>
2018-04-09 06:48:48 +0900

    Update login.php
    对应2018

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 $username = $_POST["username"];
 $password = $_POST["password"];
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,'https://yestudy.komazawa-u.ac.jp/2017/login/index.php');
+curl_setopt($ch, CURLOPT_URL,'https://yestudy.komazawa-u.ac.jp/2018/login/index.php');
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS,"username=" . $username . "&password=" . $password );
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
